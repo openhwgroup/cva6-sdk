c371b4f94db29909be08a2f009dae2906ec0645f
Andreas Korb <andreas.korb@aisec.fraunhofer.de>
2025-04-23 17:15:48 +0200

    Generate FIT image instead of legacy uImage
    Signed-off-by: Andreas Korb <andreas.korb@aisec.fraunhofer.de>

--- post_build.sh
+++ post_build.sh
@@ -3,19 +3,16 @@
 TARGET_DIR=$1
 
 if grep -q '^BR2_RISCV_32=y' ${BR2_CONFIG}; then
-    UIMAGE_LOAD_ADDRESS=0x80400000
-    UIMAGE_ENTRY_POINT=0x80400000
-    IMAGE_NAME="CV32A6Linux"
+    ITS_FILE=../tree32.its
 elif grep -q '^BR2_RISCV_64=y' ${BR2_CONFIG}; then
-    UIMAGE_LOAD_ADDRESS=0x80200000
-    UIMAGE_ENTRY_POINT=0x80200000
-    IMAGE_NAME="CV64A6Linux"
+    ITS_FILE=../tree64.its
 else
     echo "Unknown config"
     exit 1
 fi
 
-# Create the Linux image with u-boot header (uImage)
-${HOST_DIR}/bin/mkimage -A riscv -O linux -T kernel -a $UIMAGE_LOAD_ADDRESS -e $UIMAGE_ENTRY_POINT -C gzip -n $IMAGE_NAME -d "$BINARIES_DIR/Image.gz" "$BINARIES_DIR/uImage"
+# Create FIT image
+output/host/bin/mkimage -f $ITS_FILE "$BINARIES_DIR/fitImage.itb"
 
+# Create flashable sdcard.img
 support/scripts/genimage.sh -c ../genimage.cfg
